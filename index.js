40ac573d53ac02a85bd389d1393054f28fda2708
ndalilavin <vinson.steve@student.moringaschool.com>
2022-06-04 17:47:24 +0300

    commit tree 38066c0fe3d8c8aa649439d587a6b83efd454fe4

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
